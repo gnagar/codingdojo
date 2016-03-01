fbe27bf206471027b439514f4f40a3f63fb05d3e
Gaurav Nagar <gauravnagar@yahoo.com>
2016-03-01 15:38:05 +0530

    Added 4 tests to test Spares and Strikes

--- src/main/java/in/co/appworks/Game.java
+++ src/main/java/in/co/appworks/Game.java
@@ -2,14 +2,49 @@ package in.co.appworks;
 
 public class Game {
 
-	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
 
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll++] = pins;
+
 	}
 
 	public int score() {
+		int score = 0;
+		int frameIndex = 0;
+		for (int frame = 0; frame < 10; frame++) {
+
+			if (isStrike(frameIndex)) {
+				score += 10 + strikeBonus(frameIndex);
+				frameIndex++;
+			} else if (isSpare(frameIndex)) {
+				score += 10 + spareBonus(frameIndex);
+				frameIndex += 2;
+			} else {
+				score += rolls[frameIndex] + rolls[frameIndex + 1];
+				frameIndex += 2;
+			}
+
+		}
+
 		return score;
 	}
 
+	private int spareBonus(int frameIndex) {
+		return rolls[frameIndex + 2];
+	}
+
+	private int strikeBonus(int frameIndex) {
+		int i = rolls[frameIndex + 1] + spareBonus(frameIndex);
+		return i;
+	}
+
+	private boolean isStrike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
+
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+	}
 }
--- src/test/java/in/co/appworks/BowlingGameTest.java
+++ src/test/java/in/co/appworks/BowlingGameTest.java
@@ -18,23 +18,70 @@ public class BowlingGameTest {
 	public void testGutterGameWhereNoPinsAreBrokenAfterAllTries()
 			throws Exception {
 
-		int totalRolls = 20;
-
-		int pins = 0;
-
-		for (int tries = 0; tries < totalRolls; tries++) {
-			testGame.roll(pins);
-		}
+		rollMany(20, 0);
 
 		assertEquals(0, testGame.score());
 	}
-
 	@Test
-	public void testIfOnePinEachTryResultsInCorrectScore() throws Exception {
-		for (int i = 0; i < 20; i++) {
-			testGame.roll(1);
-		}
+	public void testOnePinEachTry() throws Exception {
+
+		rollMany(20, 1);
+
 		assertEquals(20, testGame.score());
 	}
+	
+	@Test
+	public void testOneSpare() throws Exception {
+		rollSpare();
+		testGame.roll(3);		
+		rollMany(17, 0);
+		
+		assertEquals(16, testGame.score());
+	}
+	
+	@Test
+	public void testTwoSpares() throws Exception {
+		rollSpare();
+		testGame.roll(5);
+		testGame.roll(0);
+		rollSpare();
+		testGame.roll(3);
+		rollMany(13, 0);
+		
+		assertEquals(36, testGame.score());
+		
+	}
+	
+	@Test
+	public void testAStrike() throws Exception {
+		rollStrike();
+		testGame.roll(1);
+		testGame.roll(6);
+		rollMany(16, 0);
+		
+		assertEquals(24, testGame.score());
+		
+	}
+	
+	@Test
+	public void testPerfectGame() throws Exception {
+		rollMany(12, 10);
+		assertEquals(300, testGame.score());
+	}
 
+	private void rollStrike() {
+		testGame.roll(10);
+	}
+
+	private void rollSpare() {
+		testGame.roll(5);
+		testGame.roll(5);
+	}
+
+	private void rollMany(int totalRolls, int pins) {
+		for (int tries = 0; tries < totalRolls; tries++) {
+			testGame.roll(pins);
+		}
+	}
+	
 }
